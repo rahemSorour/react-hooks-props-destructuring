4571d556092e645f9aef480a082b4e56306a143d
Rahem <sorourrahem17@gmail.com>
2023-09-22 05:30:07 +0100

    completed assignement

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-function MovieCard({
-  title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
-  genres,
-}) {
-  return (
-    <div className="movie-card">
-      <img src={posterSrc} alt={title} />
-      <h2>{title}</h2>
-      <small>{genres.join(", ")}</small>
-    </div>
-  );
+function MovieCard({links,  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg"}){
+
+return(
+  <div className="movie-card">
+    <img src={posterSrc} alt={posterSrc} />
+    <ul>
+     <a href='{links.github}'>{links.github}</a>
+     <a href='{links.linkedin}'>{links.linkedin}</a>
+    </ul>
+  </div>
+)
 }
 
 export default MovieCard;
